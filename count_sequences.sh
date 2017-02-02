c49c961525f37dbfebfb48014c5b965d42cbb520
KevinOluoch <kevinoluoch@github.com>
2017-02-02 09:10:05 +0300

    remove file

--- count_sequences.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#This program counts sequances in a given file
-
-echo start
-grep -c  "^echo" * 
-echo done
--- read_directory.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#This program reads a directory path from a user
-
-echo This program reads the current working directory\'s
-echo path and writes it in a file called file_path.txt
-echo
-echo The file path of this directory is 
-pwd
-pwd > file_path.txt 
